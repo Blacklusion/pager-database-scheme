e1e3c90fca983c3d41c654286abaeedad90e82e7
Leonard Scheidemantel <leonard.scheidemantel@gmail.com>
2021-06-15 17:39:35 +0200

    Add Atomic fields

--- entity/Atomic.ts
+++ entity/Atomic.ts
@@ -33,4 +33,67 @@ export class Atomic {
 
   @Column({ default: null })
   ssl_message: string
+
+  @Column({default: false})
+  location_ok: boolean;
+
+  @Column({default: false})
+  health_found: boolean
+
+  @Column({default: null})
+  health_found_message: string
+
+  @Column({default: false})
+  health_postgres_ok: boolean
+
+  @Column({default: false})
+  health_redis_ok: boolean
+
+  @Column({default: false})
+  health_chain_ok: boolean
+
+  @Column({default: false})
+  health_services_ok: boolean
+
+  @Column({default: null})
+  health_services_message: string
+
+  @Column({default: false})
+  health_total_indexed_blocks_ok: boolean
+
+  @Column({default: null})
+  health_total_indexed_blocks_message: string
+
+  @Column({default: false})
+  alive_ok: boolean
+
+  @Column({default: null})
+  alive_message: string
+
+  @Column({default: false})
+  assets_ok: boolean
+
+  @Column({nullable: true})
+  assets_ms: number;
+
+  @Column({default: null})
+  assets_message: string
+
+  @Column({default: false})
+  collections_ok: boolean
+
+  @Column({nullable: true})
+  collections_ms: number;
+
+  @Column({default: null})
+  collections_message: string
+
+  @Column({default: false})
+  schemas_ok: boolean
+
+  @Column({nullable: true})
+  schemas_ms: number;
+
+  @Column({default: null})
+  schemas_message: string
 }
